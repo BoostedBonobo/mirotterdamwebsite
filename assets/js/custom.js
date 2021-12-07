8aa2b9da634c2fe1d0ad3d473367f5cf964c6152
Janichi <jan.roozemond@gmail.com>
2021-12-08 00:14:09 +0100

    Opgeschoond code. Screenshot code aangepast voor css filter support.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,4 @@
+//url check
 window.onload = function() {
     let url = window.location.href;
     if (screen.width < 730 && url.includes('ontwerpen')){
@@ -11,7 +12,7 @@ window.onload = function() {
     }
 }
 
-//verhaallijn
+//about buttons
 function showAbout() {
     document.getElementById('about').style.display = "block";
     document.getElementById('herobox').style.display = "none";
@@ -20,7 +21,6 @@ function showAbout() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 async function kijkenBtn() {
     await sleep(160);
     document.getElementById('kijkendiv').style.display = "none";
@@ -39,45 +39,75 @@ async function showDesign() {
 
 
  //mail verzenden
- function saveConcept() {
-    if (confirm('Weet je zeker dat je het ontwerp wilt opslaan?')) {
-        html2canvas(document.getElementById('canvas'),{scrollX: 0,
-            scrollY: -window.scrollY}).then(function(canvas) {
-                saveAs(canvas.toDataURL(), 'ontwerp.png');
-            });
+//  function saveConcept() {
+//     if (confirm('Weet je zeker dat je het ontwerp wilt opslaan?')) {
+//         html2canvas(document.getElementById('canvas'),{scrollX: 0,
+//             scrollY: -window.scrollY}).then(function(canvas) {
+//                 saveAs(canvas.toDataURL(), 'ontwerp.png');
+//             });
     
-        function saveAs(uri, filename) {
-            var link = document.createElement('a');
+//         function saveAs(uri, filename) {
+//             var link = document.createElement('a');
 
-            if (typeof link.download === 'string') {
-                link.href = uri;
-                link.download = filename;
+//             if (typeof link.download === 'string') {
+//                 link.href = uri;
+//                 link.download = filename;
                 
-                document.body.appendChild(link);
-                document.getElementById('storedScreenshot').style.backgroundImage="url("+ link+")";
-                document.getElementById('ontwerpid').value=link;
+//                 document.body.appendChild(link);
+//                 document.getElementById('storedScreenshot').style.backgroundImage="url("+ link+")";
+//                 document.getElementById('ontwerpid').value=link;
         
-                // link.click();
+//                 // link.click();
         
-                // remove the link when done
-                // document.body.removeChild(link);
-            } else {
-                window.open(uri);
-            }
-        }
-    document.getElementById('contact').style.display = "block";
-    document.getElementById('design').style.display = "none";
-    }
-    else{
-    }
-}
+//                 // remove the link when done
+//                 // document.body.removeChild(link);
+//             } else {
+//                 window.open(uri);
+//             }
+//         }
+//     document.getElementById('contact').style.display = "block";
+//     document.getElementById('design').style.display = "none";
+//     }
+//     else{
+//     }
+// }
 
+//design creater confirm button
+document.getElementById('ok-button').addEventListener('click', function() {
+    if (confirm('Weet je zeker dat je klaar bent met je ontwerp?')) {
+        var node = document.getElementById('canvas');
+        domtoimage.toPng(node)
+        .then (function (dataUrl) {
+            var img = new Image();
+            img.src = dataUrl;
 
+            //block screenshot div location
+            document.getElementById('contact').style.display = "block";
+
+            //add screenshot as backgrounds styling
+            document.getElementById('storedScreenshot').style.backgroundImage="url("+ dataUrl+")";
+
+            //hide design section
+            document.getElementById('design').style.display = "none";
+
+            //add screenshot to form data for mail service
+            document.getElementById('ontwerpid').value=link;
+
+            // document.body.appendChild(img);
+            
+        })
+        .catch(function (error) {
+            console.error('Fout bij het maken van screenshot', error);
+        });
+    }
+    else{}
+});
+
+//form button
 window.onsubmit = function() { 
     alert('Versturen bericht..'); 
     document.getElementById('contact').style.display = "none";
     document.getElementById('endscreen').style.display = "block";
-    return true; };
-
-
+    return true; 
+}
 
--- ontwerpen.php
+++ ontwerpen.php
@@ -11,80 +11,55 @@ $elements = include 'assets/php/elements.php';
   <meta name="description" content="ontwerpomgeving voor mirotterdam">
   <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
   
-  <!-- return elements for design to javascript -->
+  <!-- return elements for design to javascript with php -->
   <script type="text/javascript"> 
   var elements_array = <?php echo json_encode($elements); ?>
   </script>
+  <!-- include module directly -->
   <script type="module" src="assets/js/design-maker/loadElements.js"></script>
-  <?php 
-  include('views/header.html')
-  ?>
+
+  <!-- rest of header -->
+  <?php include('views/header.html') ?>
 </head>
+
 <body>
-  <!-- Header -->
-  <header id="header" class="fixed-top ">
-  </header><!-- End Header -->
 
   <!-- Slider Section -->
   <div id="herobox" class="test" style="display:block;">
-    <?php
-      include('views/slider.html');
-    ?>
-  </div><!-- herobox-->
+    <?php include('views/slider.html'); ?> <!-- reference to slider html -->
+  </div>
+  <!-- End Slider Section-->
 
-  <!-- verhaallijn -->
+  <!-- about section(verhaallijn) -->
   <section id="about" class="about" style="display:none;">
-    <?php
-      include('views/about.html');
-    ?>
-  </section><!-- End Section -->
+    <?php include('views/about.html'); ?> <!-- reference to verhaallijn html -->
+  </section>
+  <!-- End About Section -->
 
   <!-- Design Section -->
   <section id="design" class="design" style="display:none;">
-  <?php
-   include('views/design.html');
-   ?>
-  </section><!-- End Design Section -->
+    <?php include('views/design.html'); ?> <!-- reference to designcreater html -->
+  </section>
+  <!-- End Design Section -->
 
   <!-- Contact Section  -->
   <section id="contact" class="contact" style="display:none;">
-  <?php
-   include('views/contact.html');
-   ?>
-  </section><!-- End Contact Section -->
+    <?php include('views/contact2.html'); ?> <!-- reference to contact html -->
+  </section>
+  <!-- End Contact Section -->
 
+  <!-- wip -->
   <section id="endscreen" class="endscreen" style="display:none;" style="width:100px; height:100px;">
     <h2> Bericht wordt nu verzonden.. </h2>
     <h3> Na het versturen wordt de pagina doorgeleid </h3>
     <h3> Zie de ontwerpen van de mensen voor je: </h3>
     <p> afbeeldingen </p>
-  </section><!-- End Contact Section -->
+  </section>
 
 </body>
-  <!-- ======= Footer ======= -->
+
 <footer id="footer" >
-  <div class="container">
-  </div>
+  <?php include('views/footer.html'); ?>
+</footer>
 
-  <!-- bootstrap data -->
-  <div id="preloader"></div>
-  <!-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- JS Files -->
-  <script src="assets/js/custom.js"></script>
-  <script src="assets/js/main.js"></script>
-
-  <!--ontwerpen scripts-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/Draggable.min.js"></script>
-
-  <script src="assets/js/design-maker/elements-library/ElementsMenu.js"></script>
-  <script type="module" src="assets/js/design-maker/scripts.js"></script>
-  <!--scripts-->
-
-  
-</footer><!-- End Footer -->
 </html>
